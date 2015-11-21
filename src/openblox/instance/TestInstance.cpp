d5dbdc8537521e3f4e8e77dd402e9c0ed106cc01
John M. Harris, Jr <johnmh@openmailbox.org>
2015-11-21 13:36:38 -0500

    Add TestInstance

---
+++ src/openblox/instance/TestInstance.cpp
@@ -0,0 +1,17 @@
+/*
+ * TestInstance.cpp
+ *
+ *  Created on: Nov 21, 2015
+ *      Author: johnmh
+ */
+
+#include "TestInstance.h"
+
+BEGIN_INSTANCE
+	DEFINE_CLASS(TestInstance, true, false, Instance);
+
+	TestInstance::TestInstance(){}
+
+	TestInstance::~TestInstance(){}
+
+END_INSTANCE
