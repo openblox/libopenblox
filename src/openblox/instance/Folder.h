ebe0af58c0f47b4fcc0c129d0ee70f43dcdf16f1
John M. Harris, Jr <johnmh@openmailbox.org>
2015-12-06 13:12:35 -0500

    Implement Folder

---
+++ src/openblox/instance/Folder.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2015 John M. Harris, Jr.
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenBlox.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OPENBLOX_INSTANCE_FOLDER_H_
+#define OPENBLOX_INSTANCE_FOLDER_H_
+
+#include "Instance.h"
+
+BEGIN_INSTANCE
+
+class Folder: public Instance{
+	public:
+		Folder();
+		virtual ~Folder();
+
+		DECLARE_CLASS(Folder);
+};
+
+END_INSTANCE
+
+#endif
