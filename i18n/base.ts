0d0d00ee8ee42e7d6347931c288094d78b791cc2
John M. Harris, Jr <johnmh@openmailbox.org>
2015-12-21 14:19:47 -0500

    Start i18n

---
+++ i18n/base.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>OBEngine</name>
+    <message>
+        <source>only-one-instance</source>
+        <translatorcomment>src/openblox/OBEngine.cpp:35</translatorcomment>
+        <translation type="unfinished">Only one Instance of OBEngine can exist, at one time.</translation>
+    </message>
+</context>
+</TS>
