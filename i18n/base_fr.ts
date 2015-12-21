e65d73b14485dbb46a886d038a5fa22602798109
Hippalectryon <gauthier.haas@laposte.net>
2015-12-21 22:10:00 +0100

    Added French translations to all the existing strings

--- i18n/base_fr.ts
+++ i18n/base_fr.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR">
+<TS version="2.0" language="fr_FR">
 <context>
     <name>AssetLocator</name>
     <message>
@@ -10,12 +10,12 @@
         <location filename="../src/openblox/instance/HttpService.cpp" line="50"/>
         <location filename="../src/openblox/instance/HttpService.cpp" line="97"/>
         <source>Failed to allocate memory.</source>
-        <translation type="unfinished"></translation>
+        <translation>Échec de l&apos;allocatin de la mémoire.</translation>
     </message>
     <message>
         <location filename="../src/openblox/AssetLocator.cpp" line="29"/>
         <source>Only one instance of AssetLocator can be created.</source>
-        <translation type="unfinished"></translation>
+        <translation>Une seule instance d&apos;AssetLocator peut être créée.</translation>
     </message>
 </context>
 <context>
@@ -23,12 +23,12 @@
     <message>
         <location filename="../src/openblox/ClassFactory.cpp" line="28"/>
         <source>Only one instance of ClassFactory can be created.</source>
-        <translation type="unfinished"></translation>
+        <translation>Une seule instance de ClassFactory peut être créée.</translation>
     </message>
     <message>
         <location filename="../src/openblox/ClassFactory.cpp" line="52"/>
         <source>classMaker cannot be NULL.</source>
-        <translation type="unfinished"></translation>
+        <translation>classMaker ne peut pas être NULL.</translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,8 @@
         <location filename="../src/openblox/instance/HttpService.cpp" line="236"/>
         <location filename="../src/openblox/instance/HttpService.cpp" line="275"/>
         <source>Trust check failed.</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Could be improved ... - A améliorer ...</translatorcomment>
+        <translation>Échec de l&apos;authentification.</translation>
     </message>
 </context>
 <context>
@@ -48,7 +49,7 @@
         <location filename="../src/openblox/instance/Instance.cpp" line="1295"/>
         <location filename="../src/openblox/instance/Instance.cpp" line="1323"/>
         <source>Instance expected</source>
-        <translation type="unfinished"></translation>
+        <translation>Instance attendue</translation>
     </message>
 </context>
 <context>
@@ -64,7 +65,7 @@
         <location filename="../src/openblox/type/Vector3.cpp" line="36"/>
         <location filename="../src/openblox/type/Viewport.cpp" line="36"/>
         <source>This metatable is locked</source>
-        <translation type="unfinished"></translation>
+        <translation>Cette métatable est vérouillée</translation>
     </message>
 </context>
 <context>
@@ -73,7 +74,7 @@
         <location filename="../src/openblox/OBEngine.cpp" line="35"/>
         <location filename="../src/openblox/OBEngine.cpp" line="40"/>
         <source>Only one instance of OBEngine can exist, at one time.</source>
-        <translation type="unfinished"></translation>
+        <translation>Une seule instance de OBEngine peut existed à la fois.</translation>
     </message>
     <message>
         <location filename="../src/openblox/OBEngine.cpp" line="75"/>
@@ -81,22 +82,22 @@
         <location filename="../src/openblox/OBEngine.cpp" line="111"/>
         <location filename="../src/openblox/OBEngine.cpp" line="128"/>
         <source>This OBEngine is already initialized.</source>
-        <translation type="unfinished"></translation>
+        <translation>Cet OBEngine est déjà initialisé.</translation>
     </message>
     <message>
         <location filename="../src/openblox/OBEngine.cpp" line="134"/>
         <source>If you set an Ogre::Root*, you must also set an Ogre::RenderWindow* before initializing OBEngine.</source>
-        <translation type="unfinished"></translation>
+        <translation>Si vous utilisez une Ogre::Root*, vous devez aussi mettre en place une Ogre::RenderWindow* avant d&apos;initialiser OBEngine.</translation>
     </message>
     <message>
         <location filename="../src/openblox/OBEngine.cpp" line="231"/>
         <source>INFO</source>
-        <translation type="unfinished"></translation>
+        <translation>INFO</translation>
     </message>
     <message>
         <location filename="../src/openblox/OBEngine.cpp" line="239"/>
         <source>WARN</source>
-        <translation type="unfinished"></translation>
+        <translation>ATTENTION</translation>
     </message>
 </context>
 </TS>
