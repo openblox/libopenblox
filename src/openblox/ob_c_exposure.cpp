d30bbb2c2dcd08702ae2b039ba34c408cdd15d04
John M. Harris, Jr <johnmh@openblox.org>
2016-02-13 19:02:31 -0500

    Update Makefile.in

---
+++ src/openblox/ob_c_exposure.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2016 John M. Harris, Jr.
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenBlox.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This is just here so we can test for OpenBlox using C.
+ * This is used by the reference client and server implementations,
+ * as well as the Studio software.
+ */
+
+#ifndef OPENBLOX_OB_C_EXPOSURE_CPP_
+#define OPENBLOX_OB_C_EXPOSURE_CPP_
+extern "C" {
+	void openblox_is_valid(void);
+}
+#endif
