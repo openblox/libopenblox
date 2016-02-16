e3c6c0856d131a692dbc4112026d4327935834d0
John M. Harris, Jr <johnmh@openblox.org>
2016-02-16 14:45:42 -0500

    Start abstract Mouse class

---
+++ src/openblox/instance/Mouse.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 John M. Harris, Jr.
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OpenBlox.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Mouse.h"
+
+BEGIN_INSTANCE
+	DEFINE_ABS_CLASS(Mouse, Instance);
+
+	Mouse::Mouse() : Instance(){
+		Name = ClassName;
+	}
+
+	Mouse::~Mouse(){}
+
+	Instance* Mouse::cloneImpl(Instance* newGuy){
+		Instance::cloneImpl(newGuy);
+		return newGuy;
+	}
+END_INSTANCE
