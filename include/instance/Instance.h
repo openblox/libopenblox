b8f606acd980cb6cd725a7bce0ba3ef9c849f4d5
John M. Harris <johnmh@openblox.org>
2016-06-30 16:08:44 -0400

    Add placeholder Instance class

---
+++ include/instance/Instance.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016 John M. Harris, Jr. <johnmh@openblox.org>
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with OpenBlox.	 If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+
+#ifndef OB_INST_INSTANCE
+#define OB_INST_INSTANCE
+
+namespace OB{
+	namespace Instance{
+		class Instance{
+				
+		}
+	}
+}
+
+#endif // OB_INST_INSTANCE
+
+
+// Local Variables:
+// mode: c++
+// End:
