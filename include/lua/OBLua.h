d1f2d29cd181e1aeafa73d777213373990022a97
John M. Harris <johnmh@openblox.org>
2016-08-08 15:55:00 -0400

    Start Lua implementation

---
+++ include/lua/OBLua.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2016 John M. Harris, Jr. <johnmh@openblox.org>
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with OpenBlox.	 If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef OB_LUA_OBLUA
+#define OB_LUA_OBLUA
+
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+namespace OB{
+	namespace Lua{
+		struct OBLState{
+			lua_State* L;
+			int ref;
+		};
+
+		/**
+		 * Returns a global Lua state, which is only used to handle
+		 * global variables and state management (i.e. though the
+		 * TaskScheduler).
+		 *
+		 * @returns Global a global Lua state.
+		 * @author John M. Harris, Jr.
+		 */
+		lua_State* initGlobal();
+
+		/**
+		 * Returns a Lua state under the global Lua state with the
+		 * altered OpenBlox standard library. OpenBlox provides an
+		 * altered base library, coroutine library and os library. The
+		 * other libraries loaded are as follows: table, string, math,
+		 * utf8 
+		 *
+		 * @param gL Global Lua state
+		 * @returns Lua state under the global Lua state
+		 * @author John M. Harris, Jr.
+		 */
+		lua_State* initThread(lua_State* gL);
+
+		/**
+		 * Returns a Lua state under a parent Lua state. This replaces
+		 * Lua's default coroutine creation, because OpenBlox needs to
+		 * keep track of certain metadata about each Lua state, for
+		 * example the 'script' value. Event handlers are also passed
+		 * through this.
+		 *
+		 * @param pL Parent Lua state
+		 * @returns New Lua state under the parent Lua state
+		 * @author John M. Harris, Jr.
+		 */
+		lua_State* initCoroutine(lua_State* pL);
+	}
+}
+
+#endif // OB_LUA_OBLUA
+
+// Local Variables:
+// mode: c++
+// End:
---
+++ src/lua/OBLua.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2016 John M. Harris, Jr. <johnmh@openblox.org>
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with OpenBlox.	 If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "lua/OBLua.h"
+
+#include <map>
+
+namespace OB{
+	namespace Lua{
+		void* l_alloc(void* ud, void* ptr, size_t osize, size_t nsize){
+			(void)ud;
+			(void)osize;
+
+			if(nsize == 0){
+				free(ptr);
+				return NULL;
+			}else{
+				return realloc(ptr, nsize);
+			}
+		}
+		
+		//Stores information about Lua states used by OpenBlox, for example the 'script' value.
+		static std::map<lua_State*, struct OBLState*> lStates;
+		
+		lua_State* initGlobal(){
+			//Don't put anything on the global state, its one purpose
+			//is to be the parent of coroutines.
+			return lua_newstate(l_alloc, NULL);
+		}
+
+		lua_State* initThread(lua_State* gL){
+			lua_State* L = lua_newthread(gL);
+
+			struct OBLState* LState = new struct OBLState;
+			LState->L = L;
+			LState->ref = luaL_ref(gL, LUA_REGISTRYINDEX);
+
+			lStates[L] = LState;
+
+			//Load altered standard lib
+			//luaL_requiref(L, "_G", luaopen_obbase, 1);//OB version of Lua's base lib
+			luaL_requiref(L, LUA_COLIBNAME, luaopen_coroutine, 1);
+			luaL_requiref(L, LUA_TABLIBNAME, luaopen_table, 1);
+			//luaL_requiref(L, LUA_OSLIBNAME, luaopen_obos, 1);//OB version of Lua's os lib
+			luaL_requiref(L, LUA_STRLIBNAME, luaopen_string, 1);
+			luaL_requiref(L, LUA_MATHLIBNAME, luaopen_math, 1);
+			luaL_requiref(L, LUA_UTF8LIBNAME, luaopen_utf8, 1);
+
+			lua_pop(L, 7);
+			
+			return L;
+		}
+
+		lua_State* initCoroutine(lua_State* pL){
+			//Unlike "initThread", we don't load in our altered standard library.
+			//We want this coroutine to use the environment of the parent state.
+
+			lua_State* L = lua_newthread(pL);
+
+			struct OBLState* LState = new struct OBLState;
+			LState->L = L;
+			LState->ref = luaL_ref(pL, LUA_REGISTRYINDEX);
+
+			lStates[L] = LState;
+			
+			return L;
+		}
+	}
+}
