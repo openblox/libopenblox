de7e44eb15b5ce50b35c4216657947455d3afcc3
John M. Harris <johnmh@openblox.org>
2016-11-15 13:02:15 -0500

    Implement TestInstance, a test instance class

---
+++ src/instance/TestInstance.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016 John M. Harris, Jr. <johnmh@openblox.org>
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with OpenBlox.	 If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "instance/TestInstance.h"
+
+namespace OB{
+	namespace Instance{
+		DEFINE_CLASS(TestInstance, true, false, Instance){}
+
+		TestInstance::TestInstance(){
+			Name = ClassName;
+		}
+
+		TestInstance::~TestInstance(){}
+
+		Instance* TestInstance::cloneImpl(){
+			TestInstance* ti = new TestInstance;
+			ti->Archivable = Archivable;
+			ti->Name = Name;
+			ti->ParentLocked = ParentLocked;//Hah, that'd be amusing
+			return ti;
+		}
+	}
+}
