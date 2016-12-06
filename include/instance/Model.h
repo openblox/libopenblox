204168529c1253fa9370f93bd1057db05fa0e9e1
John M. Harris <johnmh@openblox.org>
2016-12-06 12:42:49 -0500

    Add Model instance class

---
+++ include/instance/Model.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 John M. Harris, Jr. <johnmh@openblox.org>
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with OpenBlox.	 If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "instance/PVInstance.h"
+
+#ifndef OB_INST_MODEL
+#define OB_INST_MODEL
+
+namespace OB{
+	/**
+	 * Model instances are used to group other instances, mostly for
+	 * organizational purposes.
+	 *
+	 * @author John M. Harris, Jr.
+	 */
+	namespace Instance{
+		class Model: public PVInstance{
+			public:
+				Model();
+				virtual ~Model();
+
+				DECLARE_CLASS(Model);
+		};
+	}
+}
+
+#endif // OB_INST_MODEL
+
+
+// Local Variables:
+// mode: c++
+// End:
---
+++ src/instance/Model.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2016 John M. Harris, Jr. <johnmh@openblox.org>
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with OpenBlox.	 If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "instance/Model.h"
+
+#include "OBEngine.h"
+
+namespace OB{
+	namespace Instance{
+		DEFINE_CLASS_ABS_WCLONE(Model, PVInstance){
+			registerLuaClass(LuaClassName, register_lua_metamethods, register_lua_methods, register_lua_property_getters, register_lua_property_setters, register_lua_events);
+		}
+
+		Model::Model(){
+			Name = ClassName;
+
+		    OBEngine* eng = OBEngine::getInstance();
+			if(eng){
+				irr::IrrlichtDevice* irrDev = eng->getIrrlichtDevice();
+				if(irrDev){
+					irr::scene::ISceneManager* sceneMgr = irrDev->getSceneManager();
+					if(sceneMgr){
+						irrNode = sceneMgr->addEmptySceneNode();
+					}
+				}
+			}
+		}
+
+	    Model::~Model(){}
+	}
+}
