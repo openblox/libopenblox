48762b32ec1be4f3f39264c97a0716f1961fa8ac
John M. Harris <johnmh@openblox.org>
2016-12-06 13:59:17 -0500

    Add ServiceProvider instance class

---
+++ include/instance/ServiceProvider.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2016 John M. Harris, Jr. <johnmh@openblox.org>
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with OpenBlox.	 If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "instance/Instance.h"
+
+#ifndef OB_INST_SERVICEPROVIDER
+#define OB_INST_SERVICEPROVIDER
+
+namespace OB{
+	/**
+	 * ServiceProvider provides methods for objects like DataModel
+	 * that provide access to service classes like Lighting.
+	 *
+	 * @author John M. Harris, Jr.
+	 */
+	namespace Instance{
+		class ServiceProvider: public Instance{
+			public:
+			    ServiceProvider();
+				virtual ~ServiceProvider();
+
+				/**
+				 * Pretty much the same as FindFirstChildOfClass, but made 
+				 * specifically for services. Doesn't check recursively.
+				 *
+				 * @param std::string className
+				 * @returns Instance* The service requested or NULL
+				 * @author John M. Harris, Jr.
+				 */
+				virtual Instance* FindService(std::string className);
+
+				/**
+				 * Calls FindService, and attempts to create the 
+				 * service if it is not found.
+				 *
+				 * @param std::string className
+				 * @returns Instance* The service requested, or NULL if it cannot be created.
+				 * @author John M. Harris, Jr.
+				 */
+				virtual Instance* GetService(std::string className);
+
+				DECLARE_LUA_METHOD(FindService);
+				DECLARE_LUA_METHOD(GetService);
+				
+				DECLARE_CLASS(ServiceProvider);
+				
+				static void register_lua_methods(lua_State* L);
+		};
+	}
+}
+
+#endif // OB_INST_SERVICEPROVIDER
+
+
+// Local Variables:
+// mode: c++
+// End:
---
+++ src/instance/ServiceProvider.cpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2016 John M. Harris, Jr. <johnmh@openblox.org>
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with OpenBlox.	 If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "instance/ServiceProvider.h"
+
+namespace OB{
+	namespace Instance{
+		DEFINE_CLASS_ABS_WCLONE(ServiceProvider, Instance){
+			registerLuaClass(LuaClassName, register_lua_metamethods, register_lua_methods, register_lua_property_getters, register_lua_property_setters, register_lua_events);
+		}
+
+	    ServiceProvider::ServiceProvider(){
+			Name = ClassName;
+		}
+
+		ServiceProvider::~ServiceProvider(){}
+
+		Instance* ServiceProvider::FindService(std::string className){
+			for(std::vector<Instance*>::size_type i = 0; i != children.size(); i++){
+				Instance* kid = children[i];
+				if(kid != NULL){
+					if(kid->getClassName() == className){
+						return kid;
+					}
+				}
+			}
+			return NULL;
+		}
+
+		Instance* ServiceProvider::GetService(std::string className){
+			Instance* foundService = FindService(className);
+			if(foundService != NULL){
+				return foundService;
+			}
+			ob_instance::Instance* newGuy = ClassFactory::createService(className, false);
+			if(newGuy){
+				newGuy->setParent(this, false);
+				newGuy->parentLock();
+			}
+			return newGuy;
+		}
+
+		int ServiceProvider::lua_FindService(lua_State* L){
+			Instance* inst = checkInstance(L, 1);
+			if(ServiceProvider* sp = dynamic_cast<ServiceProvider*>(inst)){
+				std::string serviceName = std::string(luaL_checkstring(L, 2));
+				Instance* foundGuy = sp->FindService(serviceName);
+				if(foundGuy != NULL){
+					return foundGuy->wrap_lua(L);
+				}
+				lua_pushnil(L);
+				return 1;
+			}
+			return luaL_error(L, COLONERR, "FindService");
+		}
+
+		int ServiceProvider::lua_GetService(lua_State* L){
+			Instance* inst = checkInstance(L, 1);
+			if(ServiceProvider* sp = dynamic_cast<ServiceProvider*>(inst)){
+				std::string serviceName = std::string(luaL_checkstring(L, 2));
+				Instance* foundGuy = sp->GetService(serviceName);
+				if(foundGuy != NULL){
+					return foundGuy->wrap_lua(L);
+				}
+				lua_pushnil(L);
+				return 0;
+			}
+			return luaL_error(L, COLONERR, "GetService");
+		}
+
+		void ServiceProvider::register_lua_methods(lua_State* L){
+			Instance::register_lua_methods(L);
+
+			luaL_Reg methods[] = {
+				{"FindService", lua_FindService},
+				{"GetService", lua_GetService},
+				{NULL, NULL}
+			};
+			luaL_setfuncs(L, methods, 0);
+		}
+	}
+}
