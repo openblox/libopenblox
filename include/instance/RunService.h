a06d020ecffd53cad48887351ba424c655d841ba
John M. Harris, Jr <johnmh@openblox.org>
2017-05-06 13:09:29 -0400

    Begin implementation of RunService

---
+++ include/instance/RunService.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2017 John M. Harris, Jr. <johnmh@openblox.org>
+ *
+ * This file is part of OpenBlox.
+ *
+ * OpenBlox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenBlox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with OpenBlox.	 If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "instance/Instance.h"
+
+#ifndef OB_INST_RUNSERVICE
+#define OB_INST_RUNSERVICE
+
+#include "type/Enum.h"
+
+namespace OB{
+	namespace Instance{
+		/**
+		 * Lighting provides access to the logic and
+		 * rendering loop.
+		 *
+		 * @author John M. Harris, Jr.
+		 */
+		class RunService: public Instance{
+			public:
+			    RunService();
+				virtual ~RunService();
+
+				static void register_lua_events(lua_State* L);
+				
+				DECLARE_CLASS(RunService);
+
+				shared_ptr<Type::Event> MessageOut;
+		};
+	}
+}
+
+#endif // OB_INST_RUNSERVICE
+
+
+// Local Variables:
+// mode: c++
+// End:
