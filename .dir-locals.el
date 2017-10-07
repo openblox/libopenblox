9463225fcb309902854181b7ccaf7a0ec851c81d
John M. Harris, Jr <johnmh@openblox.org>
2017-10-07 15:20:37 -0400

    Add .dir-locals.el
    Sets up indentation properly for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((c++-mode . ((indent-tabs-mode . t)
+              (c-basic-offset . 4)
+              (tab-width . 4)
+              (eval . (progn
+                        (c-set-offset 'case-label '+))))))
