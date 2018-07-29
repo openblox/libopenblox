44398efbb1a921a0150c81b3b57702138a91ec5e
Tochigi <bailey.brownie60@gmail.com>
2018-07-29 16:09:04 -0400

    ''

--- src/utility.cpp
+++ src/utility.cpp
@@ -33,6 +33,9 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <direct.h>
+#ifdef _MSC_VER
+#include <ctime>
+#endif
 #endif
 
 namespace OB{
@@ -156,9 +159,113 @@ namespace OB{
 		WaitForSingleObject(timer, INFINITE);
 		CloseHandle(timer);
 	}
+	char* realpath(const char* path, char resolved_path[PATH_MAX]){
+		char* return_path = 0;
+
+		if(path){
+			if(resolved_path){
+				return_path = resolved_path;
+			}else{
+				return_path = (char*)malloc(PATH_MAX);
+			}
+			if(return_path){
+				size_t size = GetFullPathNameA(path, PATH_MAX, return_path, 0);
+
+				if(size > PATH_MAX){
+					if(return_path != resolved_path){
+						size_t new_size;
+
+						free(return_path);
+						return_path = (char*)malloc(size);
+
+						if(return_path){
+							new_size = GetFullPathNameA(path, size, return_path, 0);
+
+							if(new_size > size){
+								free(return_path);
+								return_path = 0;
+								errno = ENAMETOOLONG;
+							}else{
+								size = new_size;
+							}
+						}else{
+							errno = EINVAL;
+						}
+					}else{
+						return_path = 0;
+						errno = ENAMETOOLONG;
+					}
+				}
+
+				if(!size){
+					if(return_path != resolved_path){
+						free(return_path);
+					}
+
+					return_path = 0;
+
+					switch(GetLastError()){
+						case ERROR_FILE_NOT_FOUND: {
+							errno = ENOENT;
+							break;
+						}
+						case ERROR_PATH_NOT_FOUND:
+						case ERROR_INVALID_DRIVE: {
+							errno = ENOTDIR;
+							break;
+						}
+						case ERROR_ACCESS_DENIED: {
+							errno = EACCES;
+							break;
+						}
+						default: {
+							errno = EIO;
+							break;
+						}
+					}
+				}
+
+				if(return_path){
+					struct stat stat_buffer;
+
+					if(stat(return_path, &stat_buffer)){
+						if(return_path != resolved_path){
+							free(return_path);
+						}
+
+						return_path = 0;
+					}
+				}
+			}else{
+				errno = EINVAL;
+			}
+		}else{
+			errno = EINVAL;
+		}
+
+		return return_path;
+	}
+
+	char* get_current_dir_name(){
+		char* path = (char*)malloc(FILENAME_MAX);
+		return _getcwd(path, FILENAME_MAX);
+	}
+
+	void usleep(__int64 usec){
+		HANDLE timer;
+		LARGE_INTEGER ft;
+
+		ft.QuadPart = -(10*usec);
+
+		timer = CreateWaitableTimer(NULL, TRUE, NULL);
+		SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+		WaitForSingleObject(timer, INFINITE);
+		CloseHandle(timer);
+	}
+
 
 #ifdef _MSC_VER
-	int gettimeofday(struct timeval* tp, void* tzp){
+int gettimeofday(struct timeval* tp, void* tzp){
 		// Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
 		// This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
 		// until 00:00:00 January 1, 1970
@@ -177,6 +284,7 @@ namespace OB{
 		tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
 		return 0;
 	}
+
 #endif
 #endif
 }
